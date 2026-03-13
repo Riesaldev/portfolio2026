93e83d83b49011ac4e046f800278627d94f1b1ce
Riesal <ricardoea@hotmail.es>
2026-03-13 13:35:55 +0100

    feat: agregar variable de entorno para el correo de destino en el envío de EmailJS

--- env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-  readonly PUBLIC_EMAILJS_SERVICE_ID: string;
-  readonly PUBLIC_EMAILJS_TEMPLATE_ID: string;
-  readonly PUBLIC_EMAILJS_PUBLIC_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
-}
